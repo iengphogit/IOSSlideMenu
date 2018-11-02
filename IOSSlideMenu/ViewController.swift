dcf9c98fa906764532483774a627b3afaeb14bd9
iengpho <ieng.pho@gaea-sys.com>
2018-11-02 08:06:02 +0700

    done

--- IOSSlideMenu/ViewController.swift
+++ IOSSlideMenu/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slideMenuView: UIView!
+    
+    @IBOutlet weak var leadingConstrain: NSLayoutConstraint!
+    var displayMenu = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        slideMenuView.layer.shadowOpacity = 1
+        slideMenuView.layer.shadowRadius = 6
     }
 
 
+    @IBAction func openMenu(_ sender: Any) {
+        
+        if displayMenu {
+            leadingConstrain.constant = -250
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        } else {
+            leadingConstrain.constant = 0
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+        displayMenu = !displayMenu
+    }
 }
 
